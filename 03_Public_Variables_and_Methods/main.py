d4cefeb616315b41a4b21bfa1b2bc1480cda082f
Rahim Ali <rahimali1967@icloud.com>
2025-04-24 02:44:29 +0500

    create public variables and methods

--- 03_Public_Variables_and_Methods/main.py
+++ 03_Public_Variables_and_Methods/main.py
@@ -1,11 +1,11 @@
 class Car:
     def __init__(self, brand):
-        self.brand = brand  # Public variable
+        self.brand = brand 
     
-    def start(self):  # Public method
+    def start(self):  
         print(f"The {self.brand} car is starting!")
 
 # Test the class
 car1 = Car("Bmw")
-print(car1.brand)  # Access public variable
-car1.start()      # Access public method
+print(car1.brand)
+car1.start()      
