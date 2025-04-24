28d719d8260bc36147e424a3ac3d7429b077781f
Rahim Ali <rahimali1967@icloud.com>
2025-04-24 15:48:47 +0500

    create abstract classes and methods

---
+++ 09_Abstract_Classes_and_Methods/main.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+
+# Testing
+rect = Rectangle(10, 5)
+print(f"Rectangle Area: {rect.area()}") # Rectangle Area: 50
