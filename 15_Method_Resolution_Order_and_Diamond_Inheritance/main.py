36d77eed02d1282461324ed814931ae2d40d5934
Rahim Ali <rahimali1967@icloud.com>
2025-04-24 17:44:24 +0500

    create method resolution order and diamond inheritance

---
+++ 15_Method_Resolution_Order_and_Diamond_Inheritance/main.py
@@ -0,0 +1,19 @@
+class A:
+    def show(self):
+        print("Class A")
+
+class B(A):
+    def show(self):
+        print("Class B")
+
+class C(A):
+    def show(self):
+        print("Class C")
+
+class D(B, C):
+    pass
+
+# Testing
+d = D()
+d.show()
+print(D.__mro__)
