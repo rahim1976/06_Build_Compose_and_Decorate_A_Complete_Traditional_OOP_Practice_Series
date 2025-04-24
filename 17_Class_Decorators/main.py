b03059a85340e1b44eb0ac3c8f75964310e7c3b5
Rahim Ali <rahimali1967@icloud.com>
2025-04-24 18:42:51 +0500

    create class decorators

---
+++ 17_Class_Decorators/main.py
@@ -0,0 +1,14 @@
+def add_greeting(cls):
+    def greet(self):
+        return "Hello from Decorator!"
+    cls.greet = greet
+    return cls
+
+@add_greeting
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+# Testing
+person = Person("Rahim")
+print(person.greet())
